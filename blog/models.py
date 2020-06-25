ee7b6fda708f2f889380b1e64c6097423fad61dd
you <you@gmail.com>
2020-06-25 03:02:49 +0200

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):   #db table
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.title
